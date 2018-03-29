2bd9235d86bd37cc150ac9e3ca80ecad18f486ef
Wanqj <wqj97@126.com>
2018-03-29 15:59:50 +0800

    完成基本功能

--- tushareData.py
+++ tushareData.py
@@ -1,32 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import tushare as ts
+
 import pymysql
 import time
 
+conn = pymysql.Connect(
+    host='localhost',
+    user='root',
+    password='wqj9705',
+    db='ynQuant',
+    port=3306,
+    charset='utf8'
+)
+cursor = conn.cursor()
+
 while True:
-    data = ts.get_latest_news(top=20)
-    currentTime = time.ctime()
+    print("wakeing")
+    data = ts.get_latest_news(80, True)
     for index, val in data.iterrows():
-        content = ts.latest_content(val['url'])
-        # content = latest_content(url)
-        conn = pymysql.Connect(
-            host='localhost',
-            user='root',
-            password='root',
-            db='ynQuart',
-            port=3306,
-            charset='utf8'
-        )
+        sql = "INSERT INTO `news`(`classify`,`title`,`url`,`content`, `created_at`, `updated_at`) VALUES(%s,%s,%s,%s, now(), now())"
         try:
-            cursor = conn.cursor()
-            sql = "INSERT INTO `news`(`classify`,`title`,`newsTime`,`url`,`content`) VALUES(%s,%s,%s,%s,%s)"
-            cursor.execute(sql, (val['classify'], val['title'], val['time'], val['url'], content))
+            cursor.execute(sql, (val['classify'], val['title'], val['url'], val['content']))
             conn.commit()
-        except pymysql.err.IntegrityError:
-            print(pymysql.err.IntegrityError)
-            print("%s : 数据已经插入，暂未更新" % currentTime)
-        finally:
-            conn.close()
-    time.sleep(6000)
+        except Exception as e:
+            continue
+    print("sleeping")
+    time.sleep(60)
