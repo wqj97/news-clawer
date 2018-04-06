b7e682381c88b84e33c09116be1f83267e892d6a
Wanqj <wqj97@126.com>
2018-04-06 16:05:01 +0800

    新闻分析

--- tushareData.py
+++ tushareData.py
@@ -2,6 +2,20 @@ import tushare as ts
 
 import pymysql
 import time
+from QcloudApi.qcloudapi import QcloudApi
+import json
+
+Wenzhi_config = {
+    'Region': 'bj',
+    'secretId': 'AKID99jjLdep9fbPZP96VmYfRkjORy3gocKV',
+    'secretKey': 'rNSnqdmBIqB65Je73GoJIwCulXdXp9Pt',
+    'method': 'POST',
+    'SignatureMethod': 'HmacSHA1'
+}
+
+Wenzhi_action = 'TextSentiment'
+Wenzhi_module = 'wenzhi'
+service = QcloudApi(Wenzhi_module, Wenzhi_config)
 
 conn = pymysql.Connect(
     host='localhost',
@@ -15,13 +29,21 @@ cursor = conn.cursor()
 
 while True:
     print("wakeing")
-    data = ts.get_latest_news(80, True)
+    data = ts.get_latest_news(100, True)
     for index, val in data.iterrows():
-        sql = "INSERT INTO `news`(`classify`,`title`,`url`,`content`, `created_at`, `updated_at`) VALUES(%s,%s,%s,%s, now(), now())"
+        print('processing {}/{}'.format(index + 1, len(data)))
+        sql = "SELECT EXISTS(SELECT id FROM news WHERE url = '%s')".format(val['url'])
+        cursor.execute(sql)
+        if cursor.fetchone()[0] == 1:
+            continue
+        analysis = service.call(Wenzhi_action, {'content': val['content']}).decode('utf-8')
+        sql = "INSERT INTO news (title, content, news_time, classify,url,analysis, created_at, updated_at) VALUES ('{}', '{}', date_format(concat(year(now()), '-{}'), '%Y-%m-%d %H:%i'), '{}', '{}', '{}' , now(), now())".format(
+            val['title'], val['content'], val['time'], val['classify'], val['url'], analysis)
         try:
-            cursor.execute(sql, (val['classify'], val['title'], val['url'], val['content']))
+            cursor.execute(sql)
             conn.commit()
         except Exception as e:
+            print(e)
             continue
     print("sleeping")
     time.sleep(60)
