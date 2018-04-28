65b4f8fc42744e7d0a2e0679ef9fb4221e9f254b
Wanqj <wqj97@126.com>
2018-04-28 15:52:21 +0800

    2018-04-28 修复一个bug, 会导致重复录入新闻

--- tushareData.py
+++ tushareData.py
@@ -18,7 +18,7 @@ Wenzhi_module = 'wenzhi'
 service = QcloudApi(Wenzhi_module, Wenzhi_config)
 
 conn = pymysql.Connect(
-    host='localhost',
+    host='139.199.124.15',
     user='root',
     password='wqj9705',
     db='ynQuant',
@@ -27,14 +27,19 @@ conn = pymysql.Connect(
 )
 cursor = conn.cursor()
 
+number_to_claw = input('输入想要抓取的新闻数, 默认为10\r\n')
+if not number_to_claw:
+    number_to_claw = 10
+
 while True:
     print("wakeing")
-    data = ts.get_latest_news(100, True)
+    data = ts.get_latest_news(number_to_claw, True)
     for index, val in data.iterrows():
         print('processing {}/{}'.format(index + 1, len(data)))
-        sql = "SELECT EXISTS(SELECT id FROM news WHERE url = '%s')".format(val['url'])
+        sql = "SELECT EXISTS(SELECT id FROM news WHERE url = '{}')".format(val['url'])
         cursor.execute(sql)
-        if cursor.fetchone()[0] == 1:
+        fetch_result = cursor.fetchone()[0]
+        if fetch_result == 1:
             continue
         analysis = service.call(Wenzhi_action, {'content': val['content']}).decode('utf-8')
         sql = "INSERT INTO news (title, content, news_time, classify,url,analysis, created_at, updated_at) VALUES ('{}', '{}', date_format(concat(year(now()), '-{}'), '%Y-%m-%d %H:%i'), '{}', '{}', '{}' , now(), now())".format(
@@ -46,4 +51,4 @@ while True:
             print(e)
             continue
     print("sleeping")
-    time.sleep(60)
+    time.sleep(120)
